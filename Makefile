23a548f50bfaf1f5ff5656fc083a1abfc349b3b0
Seth Kushniryk <seth@sulu.sethkush.com>
2016-03-04 20:40:57 -0800

    lots of stuff

--- Makefile
+++ Makefile
@@ -1,39 +1,81 @@
-# Makefile for startsdl.c
-
-# Use the gcc compile program and produce a lot of warning messages about
-# questionable coding practices:
-CC  = gcc -Wall
-
-# Name the binary program that will be created:
-BIN = chip8
-
-# `sdl-config --cflags` gives the following options to the C compiler:
-# -I/usr/include/SDL -D_REENTRANT
-# This says to include the /usr/include/SDL directory, which defines
-# a.o. SDL_Init and holds references to all other SDL functions you use in
-# your program and which can be found in the same directory.
-# -D acts like #define, it sets a variable so as the program will build in the
-# correct way on your system.  It is used by functions in /usr/include.
-CFLAGS=`sdl2-config --cflags`
-
-# Linking with libraries: `sdl-config --libs` gives the following output:
-# -L/usr/lib -Wl,-rpath,/usr/lib -lSDL -lpthread
-# -L looks for libs in a particular directory; -l specifies individual
-# libraries.
-LDFLAGS=`sdl2-config --libs`
-
-# "all" is just a name, which points to the objectfiles to be processed.
-# If it is the first target, it is also the default.
-all: $(BIN)
-
-# Particular things to do for each part of the program.  This has only one part,
-# in which we define the object file and to process it with the appropriate
-# options.
-$(BIN): chip8.o
-	$(CC) chip8.o \
-	-o $(BIN) $(LDFLAGS)
-
-# Stuff to clean up when make is called with this argument: remove object files.
-clean:
-	@rm -f *.o
-	@rm $(BIN)
+############################################################################
+# 'A Generic Makefile for Building Multiple main() Targets in $PWD'
+# Author:  Robert A. Nader (2012)
+# Email: naderra at some g
+# Web: xiberix
+############################################################################
+#  The purpose of this makefile is to compile to executable all C source
+#  files in CWD, where each .c file has a main() function, and each object
+#  links with a common LDFLAG.
+#
+#  This makefile should suffice for simple projects that require building
+#  similar executable targets.  For example, if your CWD build requires
+#  exclusively this pattern:
+#
+#  cc -c $(CFLAGS) main_01.c
+#  cc main_01.o $(LDFLAGS) -o main_01
+#
+#  cc -c $(CFLAGS) main_2..c
+#  cc main_02.o $(LDFLAGS) -o main_02
+#
+#  etc, ... a common case when compiling the programs of some chapter,
+#  then you may be interested in using this makefile.
+#
+#  What YOU do:
+#
+#  Set PRG_SUFFIX_FLAG below to either 0 or 1 to enable or disable
+#  the generation of a .exe suffix on executables
+#
+#  Set CFLAGS and LDFLAGS according to your needs.
+#
+#  What this makefile does automagically:
+#
+#  Sets SRC to a list of *.c files in PWD using wildcard.
+#  Sets PRGS BINS and OBJS using pattern substitution.
+#  Compiles each individual .c to .o object file.
+#  Links each individual .o to its corresponding executable.
+#
+###########################################################################
+#
+PRG_SUFFIX_FLAG := 0
+#
+LDFLAGS := `sdl2-config --libs`
+CFLAGS_INC := `sdl2-config --cflags` `pkg-config --cflags --libs gtk+-3.0` -std=c99 -ggdb
+CFLAGS := -g -Wall $(CFLAGS_INC)
+BIN := chip8
+#
+## ==================- NOTHING TO CHANGE BELOW THIS LINE ===================
+##
+SRCS := $(wildcard *.c)
+PRGS := $(patsubst %.c,%,$(SRCS))
+PRG_SUFFIX=.exe
+BINS := $(patsubst %,%$(PRG_SUFFIX),$(PRGS))
+## OBJS are automagically compiled by make.
+#OBJS := $(patsubst %,%.o,$(PRGS))
+##
+all : $(BINS)
+##
+## For clarity sake we make use of:
+#.SECONDEXPANSION:
+#OBJ = $(patsubst %$(PRG_SUFFIX),%.o,$@)
+#ifeq ($(PRG_SUFFIX_FLAG),0)
+#        BIN = $(patsubst %$(PRG_SUFFIX),%,$@)
+#else
+#        BIN = $@
+#endif
+## Compile the executables
+%$(PRG_SUFFIX) : $(OBJS)
+	$(CC) $(SRCS) $(CFLAGS) $(LDFLAGS) -o $(BIN)
+##
+## $(OBJS) should be automagically removed right after linking.
+##
+veryclean:
+#ifeq ($(PRG_SUFFIX_FLAG),0)
+#	$(RM) $(PRGS)
+#else
+#	$(RM) $(BINS)
+#endif
+##
+rebuild: veryclean all
+##
+## eof Generic_Multi_Main_PWD.makefile
--- chip8.c
+++ chip8.c
@@ -1,61 +1,190 @@
 #include <SDL2/SDL.h>
+#include <inttypes.h>
 #include <stdio.h>
+#include "nfd.h"
+#include "common.h"
+#include "nfd_common.h"
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
 
+// Function prototypes:
+int start_video(SDL_Window**, SDL_Surface**);
+int load_font(uint8_t*);
+int open_program(uint8_t*, SDL_Window*);
 
 int main(int argc, char* argv[]) {
+    
+    // SDL and control flow Variables:
+    SDL_Window* window;  // Window pointer
+    SDL_Surface* screen; // Screen pointer
+    SDL_Event e;         // Event handler
+    int quit = 0;        // Quit flag
+    int run_emu = 0;     // Flag tells whether or not CPU should run code
+    const uint8_t *state = SDL_GetKeyboardState(NULL); // Keyboard state array
 
+    // Emulation Variables:
+    uint16_t opcode = 0;  // Opcodes are 2 bytes long
+    uint8_t memory[4096]; // 4K of memory
+    uint8_t V[16];        // 16 8-bit registers
+    uint16_t I = 0;       // Index register
+    uint16_t pc = 0x200;  // Program counter
+    uint8_t gfx[64 * 32]; // Array containing screen state
+    uint8_t delay_timer;  // 60 Hz timer register
+    uint8_t sound_timer;  // 60 Hz timer that sounds buzzer at 0
+    uint16_t stack[16];   // 16 level stack for subroutine jump markers
+    uint16_t sp = 0;      // Stack pointer - which level of the stack is used
+    uint8_t key[16];      // Keypad state
 
-  SDL_Window* window;
-  SDL_Surface* screen;
-
-
-  SDL_Init(SDL_INIT_VIDEO);
-
-  //Main loop flag
-  int quit = 0;
-
-  //Event handler
-  SDL_Event e;
-
-  window = SDL_CreateWindow(
-	  "CHIP 8 Emulator",                 // window title
-	  SDL_WINDOWPOS_UNDEFINED,           // initial x position
-	  SDL_WINDOWPOS_UNDEFINED,           // initial y position
-	  640,                               // width, in pixels
-	  320,                               // height, in pixels
-	  SDL_WINDOW_OPENGL                  // flags - see below
-	  );
-
-  if (window == NULL) {
-    // In the case that the window could not be made...
-    printf("Could not create window: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  // Set the surface on which we will render the screen
-  screen = SDL_GetWindowSurface( window );
-  
-  // Fill it with black
-  SDL_FillRect(
-	       screen,
-	       NULL,
-	       SDL_MapRGB( screen->format, 0, 0, 0 )
-	       );
-  
-  
-  while( !quit ) {
-    // Update the screen
-    SDL_UpdateWindowSurface( window );
-    while( SDL_PollEvent( &e ) != 0 ) {
-      //User requests quit
-      if( e.type == SDL_QUIT ) {
-	quit = 1;
-      }
+    // Call start_video with pointers to the screen and window references:
+    if( start_video(&window, &screen) != 0 ) {
+	return 1;
     }
-  }
-  
-  // kill the window and clean up
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return 0;
+    if( load_font(memory) != 0 ) {
+	return 1;
+    }
+
+    // Main loop:
+    while( !quit ) {
+
+	// Check for open key:
+	if (state[SDL_SCANCODE_O]) {
+	    printf("O is pressed\n");
+	    while(state[SDL_SCANCODE_O]) {
+		SDL_PumpEvents();
+	    }
+	    if(open_program(memory, window) != 0) {
+		return 1;
+	    }
+	    run_emu = 1;
+	}
+
+	if(run_emu){
+	    // Do a CPU cycle
+	}
+	
+     	// Update the screen:
+	SDL_UpdateWindowSurface( window );
+	
+	// Check for events:
+	while( SDL_PollEvent( &e ) != 0 ) {
+	    // User requests quit:
+	    if( e.type == SDL_QUIT ) {
+		quit = 1;
+	    }
+	}
+    }
+    
+    // kill the window and clean up
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
+
+/* 
+   This function accepts a window and a screen by reference.  It then
+   initializes them and makes the screen black.  It is called only at the start
+   of the program.
+*/
+int start_video(SDL_Window** window, SDL_Surface** screen) {
+	
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    *window = SDL_CreateWindow(
+	"CHIP 8 Emulator",                 // window title
+	SDL_WINDOWPOS_UNDEFINED,           // initial x position
+	SDL_WINDOWPOS_UNDEFINED,           // initial y position
+	640,                               // width, in pixels
+	320,                               // height, in pixels
+	SDL_WINDOW_OPENGL                  // flags
+	);
+    
+    if (*window == NULL) {
+	// In the case that the window could not be made...
+	printf("Could not create window: %s\n", SDL_GetError());
+	return 1;
+    }
+    
+    // Set the surface on which we will render the screen
+    *screen = SDL_GetWindowSurface( *window );
+    
+    // Fill it with black
+    SDL_FillRect(
+	*screen,
+	NULL,
+	SDL_MapRGB( (*screen)->format, 0, 0, 0 )
+	);
+
+    return 0;
+}
+
+int load_font(uint8_t* memory) {
+    // CHIP8 fontset:
+    uint8_t chip8_fontset[80] =
+	{ 
+	    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
+
+    // load it into the CHIP8 memory
+    for(int i = 0; i < 80; ++i) {
+	memory[i] = chip8_fontset[i];
+    }
+    
+    return 0;
+}
+
+int open_program(uint8_t* memory, SDL_Window* window) {
+    nfdchar_t *outPath = NULL;
+    nfdresult_t result = NFD_OpenDialog( NULL, NULL, &outPath );
+    if ( result == NFD_OKAY ) {
+	struct stat st;
+	stat(outPath, &st);
+	printf("%lu\n", st.st_size);
+	if(st.st_size > 3000) {
+	   return 1;
+	}
+	// load into memory
+	FILE *fp;
+	unsigned char buffer[3000];
+	for(int i=0; i<3000; i++) {
+	    buffer[i]=0;
+	}
+	fp=fopen(outPath, "rb");
+	if (fp) {
+	    fread(buffer, 3000, 1, fp);
+	    fclose(fp);
+	    //for(int i = 0; i < 3000; ++i) {
+	    //memory[i + 512] = buffer[i];
+	    //}
+	}
+	else {
+	    return 1;
+	}
+	return 0;
+    }
+    else if ( result == NFD_CANCEL ) {
+	printf("cancel\n");
+	return 0;
+    }
+    else {
+        printf("Error: %s\n", NFD_GetError() );
+	return 1;
+    }
+}
+
+
